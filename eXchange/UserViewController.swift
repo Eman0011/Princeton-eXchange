7a6a9f9153eba5bfe8976375ed712a5051c6778e
DANIELLE PINTZ <DANDAN274@GMAIL.COM>
2016-04-06 21:56:05 -0400

    Dani added upcoming button

--- eXchange/UserViewController.swift
+++ eXchange/UserViewController.swift
@@ -14,11 +14,14 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet var eXchangeBanner: UIImageView!
     @IBOutlet var historyButton: UIButton!
     @IBOutlet var unfinishedButton: UIButton!
-    
+    @IBOutlet var upcomingButton: UIButton!
     
     var historyData: [eXchange] = []
     var unfinishedXData: [eXchange] = []
+    var upcomingData: [eXchange] = []
     var historySelected = true
+    var unfinishedSelected = false
+    var upcomingSelected = false
     let formatter = NSDateFormatter()
     var daysLeft = 0
     
@@ -32,11 +35,13 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
         historyButton.backgroundColor = UIColor.orangeColor()
         unfinishedButton.layer.cornerRadius = 5
         unfinishedButton.backgroundColor = UIColor.blackColor()
+        upcomingButton.layer.cornerRadius = 5
+        upcomingButton.backgroundColor = UIColor.blackColor()
         
         formatter.dateFormat = "MM-dd-yyyy, hh:mm a"
         formatter.AMSymbol = "am"
         formatter.PMSymbol = "pm"
-
+        
         daysLeft = getDaysLeft()
         self.loadMockData()
         
@@ -54,17 +59,13 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
         let Meaghan = Student(name: "Meaghan O'Neill", netid: "", club: "Ivy", proxNumber: "")
         let Sumer = Student(name: "Sumer Parikh", netid: "", club: "Cap & Gown", proxNumber: "")
         let James = Student(name: "James Almeida", netid: "", club: "Cap & Gown", proxNumber: "")
-
+        
         let today = NSDate()
         print(formatter.stringFromDate(today))
         
         
         let x1 = eXchange(host: Emanuel, guest: Sumer,  type: "Lunch")
-        print("test")
-        print(x1.meal1.date)
         x1.meal1.date = formatter.dateFromString("3-7-2016, 1:30 pm")!
-        print(x1.meal1.date)
-        print("end test")
         let m1 = Meal(date: NSDate(), type: "Lunch", host: Sumer, guest: Emanuel)
         x1.meal2 = m1
         x1.meal2?.date = formatter.dateFromString("3-22-2016, 12:00 pm")!
@@ -81,25 +82,45 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
         let x3 = eXchange(host: Emanuel, guest: Danielle, type:  "Dinner")
         x3.meal1.date = formatter.dateFromString("3-14-2016, 6:30 pm")!
         unfinishedXData.append(x3)
+        
+        let x4 = eXchange(host: Emanuel, guest: James, type:  "Dinner")
+        x4.meal1.date = formatter.dateFromString("4-20-2016, 6:30 pm")!
+        upcomingData.append(x4)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Button Actions
+    
+    @IBAction func upcomingButtonPressed(sender: AnyObject) {
+        historySelected = false
+        unfinishedSelected = false
+        upcomingSelected = true
+        upcomingButton.backgroundColor = UIColor.orangeColor()
+        historyButton.backgroundColor = UIColor.blackColor()
+        unfinishedButton.backgroundColor = UIColor.blackColor()
+        tableView.reloadData()
+    }
     @IBAction func historyButtonPressed(sender: AnyObject) {
         historySelected = true
+        unfinishedSelected = false
+        upcomingSelected = false
         historyButton.backgroundColor = UIColor.orangeColor()
         unfinishedButton.backgroundColor = UIColor.blackColor()
+        upcomingButton.backgroundColor = UIColor.blackColor()
         tableView.reloadData()
     }
     
     @IBAction func unfinishedButtonPressed(sender: AnyObject) {
         historySelected = false
+        unfinishedSelected = true
+        upcomingSelected = false
         unfinishedButton.backgroundColor = UIColor.orangeColor()
         historyButton.backgroundColor = UIColor.blackColor()
+        upcomingButton.backgroundColor = UIColor.blackColor()
         tableView.reloadData()
     }
     
@@ -116,8 +137,12 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if historySelected {
             return historyData.count
-        } else {
+        }
+        else if unfinishedSelected {
             return unfinishedXData.count
+        }
+        else {
+            return upcomingData.count
         }
     }
     
@@ -128,29 +153,39 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "MMM dd, yyyy"
         var student: Student
-            if historySelected {
-                let exchange = historyData[indexPath.row]
-                student = exchange.guest
-                cell.nameLabel.text = exchange.guest.name
-                let meal2String: String
-                if (exchange.meal2 == nil) {
-                    meal2String = "MEAL WAS NOT INITIALIZED"
-                } else {
-                    meal2String = dateFormatter.stringFromDate(exchange.meal2!.date)
-                }
-                cell.meal1Label.text = "Meal 1: " + dateFormatter.stringFromDate(exchange.meal1.date)
-                cell.meal2Label.text = "Meal 2: " + meal2String
+        if historySelected {
+            let exchange = historyData[indexPath.row]
+            student = exchange.guest
+            cell.nameLabel.text = exchange.guest.name
+            let meal2String: String
+            if (exchange.meal2 == nil) {
+                meal2String = "MEAL WAS NOT INITIALIZED"
             } else {
-                let exchange = unfinishedXData[indexPath.row]
-                student = exchange.guest
-                cell.nameLabel.text = "Meal eXchange with " + exchange.guest.name + "."
-                cell.meal1Label.text = "\(daysLeft) days left to complete!"
-                cell.meal2Label.text = ""
+                meal2String = dateFormatter.stringFromDate(exchange.meal2!.date)
             }
+            cell.meal1Label.text = "Meal 1: " + dateFormatter.stringFromDate(exchange.meal1.date)
+            cell.meal2Label.text = "Meal 2: " + meal2String
+        }
+            
+        else if unfinishedSelected {
+            let exchange = unfinishedXData[indexPath.row]
+            student = exchange.guest
+            cell.nameLabel.text = "Meal eXchange with " + exchange.guest.name + "."
+            cell.meal1Label.text = "\(daysLeft) days left to complete!"
+            cell.meal2Label.text = ""
+        }
+        else {
+            let exchange = upcomingData[indexPath.row]
+            student = exchange.guest
+            cell.nameLabel.text = "You have scheduled a meal eXchange with " + exchange.guest.name + "."
+            cell.meal1Label.text = ""
+            cell.meal2Label.text = "For " + exchange.meal1.type + " on " + dateFormatter.stringFromDate(exchange.meal1.date)
+            
+        }
         cell.studentImage.image = UIImage(named: student.imageName)
         return cell
     }
-
+    
     /* calculate the number of days left to complete meal eXchange */
     func getDaysLeft() -> Int {
         let today = NSDate()
@@ -161,38 +196,38 @@ class UserViewController: UIViewController, UITableViewDelegate, UITableViewData
         let xEnd = range.length
         return xEnd - xStart
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-    }
-    */
     
     /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-    // Delete the row from the data source
-    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-    }
-    */
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
     
     
     
     
     /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
 }
